da99b7bf1eb845cf968ae37712d65218085a0399
li354886 <zl132@georgetown.edu>
2015-01-25 23:50:53 -0500

    Generic Binary Search questions

---
+++ BinarySearch/SearchInsertPosition.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int searchInsert(int[] A, int target) {
+        int low = 0;
+        int high = A.length - 1;
+        int mid = low + (high - low)/2;
+        while(low <= high){
+            mid = low + (high - low)/2;
+            if(A[mid] == target){
+                return mid;
+            }else{
+                if(A[mid] < target){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return low;
+    }
+}
---
+++ BinarySearch/Searh2DMatrix.java
@@ -0,0 +1,42 @@
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return false;
+        }
+        
+        int l = 0;
+        int r = matrix.length - 1;
+        while(l<=r){
+            int mid = (l + r)/2;
+            if(matrix[mid][0] == target){
+                return true;
+            }
+            if(matrix[mid][0] > target){
+                r = mid - 1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        
+        int row = r;
+        if(row < 0){
+            return false;
+        }
+        l = 0;
+        r = matrix[0].length - 1;
+        while(l <= r){
+            int mid = (l+r)/2;
+            if(matrix[row][mid] == target){
+                return true;
+            }else{
+                if(matrix[row][mid] > target){
+                    r = mid - 1;
+                }else{
+                    l = mid + 1;
+                }
+            }
+        }
+        return false;
+    }
+}
