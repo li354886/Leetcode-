a8b64409f718e9376e505da2e4c2fe0f1e2a844b
Zhengning Li <zl132@georgetown.edu>
2016-07-10 22:22:12 -0400

    new submission

---
+++ Tree/BinaryTreeUpsideDown.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        return reverse(root);
+    }
+    private TreeNode reverse(TreeNode root) {
+        if (root == null || root.left == null) {
+            return root;
+        }
+        TreeNode newRoot = reverse(root.left);
+        root.left.left = root.right;
+        root.left.right = root;
+        root.left = null;
+        root.right = null;
+        return newRoot;
+    }
+}
